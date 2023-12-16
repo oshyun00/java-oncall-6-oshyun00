d8f455094d4a0de767989799cf5606030cfd824c
osukhyun00 <osh123chm@gmail.com>
2023-12-16 18:27:57 +0900

    refactor: 들여쓰기 정리

--- src/main/java/oncall/utils/ConstantUtils.java
+++ src/main/java/oncall/utils/ConstantUtils.java
@@ -2,15 +2,15 @@ package oncall.utils;
 
 public class ConstantUtils {
     public static final String[] DAY_OF_WEEK_NAME = {"월", "화", "수", "목", "금", "토", "일"};
-public static final int COUNT_OF_DAY_OF_WEEK = 7;
-public static final String PRINT_HOLIDAY = "%s월 %d일 %s(휴일) %s\n";
-public static final String PRINT_NORMAL_DAY = "%s월 %d일 %s %s\n";
-public static final String ERROR_MESSAGE = "유효하지 않은 입력 값입니다. 다시 입력해 주세요.";
-public static final int MIN_MONTH = 1;
-public static final int MAX_MONTH = 12;
-public static final String START_MESSAGE = "비상 근무를 배정할 월과 시작 요일을 입력하세요>";
-public static final String GET_WEEKDAY_MESSAGE = "평일 비상 근무 순번대로 사원 닉네임을 입력하세요>";
-public static final String GET_WEEKEND_MESSAGE = "휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>";
-public static final String ERROR_HEADER = "[ERROR] ";
+    public static final int COUNT_OF_DAY_OF_WEEK = 7;
+    public static final String PRINT_HOLIDAY = "%s월 %d일 %s(휴일) %s\n";
+    public static final String PRINT_NORMAL_DAY = "%s월 %d일 %s %s\n";
+    public static final String ERROR_MESSAGE = "유효하지 않은 입력 값입니다. 다시 입력해 주세요.";
+    public static final int MIN_MONTH = 1;
+    public static final int MAX_MONTH = 12;
+    public static final String START_MESSAGE = "비상 근무를 배정할 월과 시작 요일을 입력하세요>";
+    public static final String GET_WEEKDAY_MESSAGE = "평일 비상 근무 순번대로 사원 닉네임을 입력하세요>";
+    public static final String GET_WEEKEND_MESSAGE = "휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>";
+    public static final String ERROR_HEADER = "[ERROR] ";
 
 }
